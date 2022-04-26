51475a93471f8b875463b2f006793cea96afaebf
Junayed Ahmed <junayedahmed11099@gamil.com>
2022-04-26 12:20:24 +0600

    arithmentic

---
+++ arithmentic-opratpr.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 20;
+console.log(a+b)
+console.log(b-a)
+console.log(b*a)
+console.log(b/a)
+var c = 100;
+var d = 3;
+console.log(c/d)
+console.log(c%d)
+var f = 10;
+f++;
+console.log(f)
+var j = 10;
+j--;
+console.log(j)
+var o = 50;
+++o;
+--o;
+console.log(o)
